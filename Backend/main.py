91e47f116a3fd921a7d6da1bb03c8468f77c2273
Gaspar Morales <gasparmorales1@gmail.com>
2024-06-08 18:48:46 -0300

    levanta bien el frontend

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import jwt_auth_user, usuario, restaurante
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:80",  # Cambia este origen si es necesario
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #routers
 app.include_router(usuario.router)
